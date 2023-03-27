b199690a6d120930897d717c3681e5056ecfa33b
modzyyreal <127398649+modzyyreal@users.noreply.github.com>
2023-03-27 07:46:31 +0000

    Update Song.lua

--- src/Song.lua
+++ src/Song.lua
@@ -142,13 +142,23 @@ function Song:_parse(event)
     ]]
     local eventName = event[1]
 
-    if (eventName == "set_tempo") then
+   if (eventName == "set_tempo") then
         self._usPerBeat = event[3]
         print("tempo changed to " .. event[3])
-    if (eventName == "note") then
+    elseif (eventName == "note") then
+        if self.MissPercent > 0 then
+            return
+            task.spawn(function()
+                local random = Random.new()
+                local random1 = chance(self.MissPercent)
+                local random2 = chance(self.MissPercent)
+                local random3 = chance(self.MissPercent)
+                if random1 then task.wait(math.random(0.1, 0.5)) end
+                if not random3 then Input.Hold(not random2 and event[5] or event[5] + random:NextInteger(-1,1), event[3]  * (self._usPerBeat / self.Timebase / 1000000), event[6]) end
+            end)
+        else
             Input.Hold(event[5], event[3]  * (self._usPerBeat / self.Timebase / 1000000))
         end
-      end
         
     elseif (eventName == "control_change") then
         if event[4] == 64 then
