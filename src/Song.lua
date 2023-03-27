77f18c7c1a944fefe468809144d8e3df6d4c6fda
modzyyreal <127398649+modzyyreal@users.noreply.github.com>
2023-03-27 07:41:58 +0000

    Update Song.lua

--- src/Song.lua
+++ src/Song.lua
@@ -146,7 +146,7 @@ function Song:_parse(event)
         self._usPerBeat = event[3]
         print("tempo changed to " .. event[3])
     elseif (eventName == "note") then
-            Input.Hold(event[5], event[3]  * (self._usPerBeat / self.Timebase / 1000000)
+            Input.Hold(event[5], event[3]  * (self._usPerBeat / self.Timebase / 1000000))
         end
       end
         
